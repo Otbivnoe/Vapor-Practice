cd2b0cb4e0a7f9da5a87668e2aa841b99b8df303
Nikita Ermolenko <gnod94@gmail.com>
2017-02-11 18:24:23 +0600

    Get practice by two raywenderlich screencasts - https://videos.raywenderlich.com/screencasts/server-side-swift-with-vapor-getting-started - https://videos.raywenderlich.com/screencasts/server-side-swift-with-vapor-templating-with-leaf

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -2,4 +2,38 @@ import Vapor
 
 let drop = Droplet()
 
+drop.get("first") { requst in
+    return "Hello, world!"
+}
+
+drop.get("path", "1") { requst in
+    return JSON(["result" : 1])
+}
+
+drop.get("Leaf", String.self) { request, name in
+    return try drop.view.make("hello", ["name" : name])
+}
+
+drop.get("justUsers") { request in
+    let users = try ["Nikita", "Artem", "Anton"].makeNode()
+    return try drop.view.make("users", ["users" : users])
+}
+
+drop.get("usersWithEmails") { request in
+    let users = try [
+            ["name" : "Nikita", "email" : "Test1@mail.ru"].makeNode(),
+            ["name" : "Artem",  "email" : "Test2@mail.ru"].makeNode(),
+            ["name" : "Anton",  "email" : "Test3@mail.ru"].makeNode()
+        ].makeNode()
+    return try drop.view.make("usersEmail", ["users" : users])
+}
+
+drop.get("helloAwesome") { request in
+    guard let sayHello = request.data["sayHello"]?.bool else {
+        throw Abort.badRequest
+    }
+    return try drop.view.make("helloAwesome", Node(node: ["sayHello": sayHello.makeNode()]))
+}
+
+
 drop.run()
